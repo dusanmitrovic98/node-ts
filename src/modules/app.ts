e6c1281f3198a88040ea1fec3d4166125387de1b
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-10 13:00:33 +0200

    Update app.ts

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const WEBSITE_URL = process.env.WEBSITE_URL || "http://example.com";
+
 async function app(): Promise<void> {}
 
 export default app;
